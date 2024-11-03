5e5e67c0092ab986243715258c6fe1d428bcd55d
b4c1c <105210965+b4c1c@users.noreply.github.com>
2024-11-03 13:41:49 +0100

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -16,8 +16,8 @@ const Info = () => {
             <h4>b4c1c</h4>
             <p>frontend developer</p>
             <div className="btns-container">
-                <a href="https://www.linkedin.com/in/goran-b-59a170134/"  className="btns btn-email"> <FaMailBulk />Email</a>
-                <a href="https://www.linkedin.com/in/goran-b-59a170134/" className="btns btn-linkedin"> <FaLinkedin />LinkedIn</a>
+                <a href="https://github.com/b4c1c"  className="btns btn-email"> <FaMailBulk />Email</a>
+                <a href="https://github.com/b4c1c" className="btns btn-linkedin"> <FaLinkedin />LinkedIn</a>
             </div>
         </div>
     );
